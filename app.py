5a8d10f5527267988b13ee6afd61f32c7ed31837
SoleDuck <wis2017wa@gmail.com>
2025-01-12 12:30:47 -0800

    Sigma

--- app.py
+++ app.py
@@ -1,54 +1,72 @@
-
-
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
+#Authenticator
 app = Flask(__name__)
+app.secret_key = "RNX73oMEeL5ShKoaHiiZkw"
+
+
+
+#Post database model
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+
+app.config['SQLALCHEMY_BINDS'] = {
+
+    'users': 'sqlite:///users.db'
+
+}
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class BlogPost(db.Model): # Model for the database
-    id = db.Column(db.Integer, primary_key = True) # Different Data Type, Primary Key means must be different from other id's
-    title = db.Column(db.String(100), nullable = False) #character limit, cannot be blank
-    content = db.Column(db.Text, nullable = False) # No limit, cannot be blank
-    author = db.Column(db.String(20), nullable = False, default = 'N/A') # 20 char limit, but theres a default if not put in
-    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow) #DateTime data type is just the time, default is the UTC (Universal Coordinated Time)
-    
-    def __repr__(self): #will print whenever a new blog post is created (the constructor)
-        return 'Blog post ' + str(self.id)
+#Database model
+class User(db.Model):
+    __bind_key__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(25), unique = True, nullable = False)
+    password = db.Column(db.String(150), nullable = False)
+
+    def set_password(self, password):
+        self.password_hash = generate_passowrd_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+#Blog model
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    content = db.Column(db.Text, nullable = False)
+    author = db.Column(db.String(20), nullable = False, default = 'N/A')
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog post ' + str(self.id) 
+
+all_posts = []
 
 @app.route('/')
 def index():
+    if "username" in session:
+        return redirect(url_for('dashboard'))
     return render_template('index.html')
+
 @app.route('/posts', methods=['GET', 'POST'])
 def posts():
-    
+
     if request.method == 'POST':
         post_title = request.form['title']
         post_content = request.form['content']
-        posts_author = request.form['author']
-        new_post = BlogPost(title=post_title, content=post_content, author = posts_author)
+        post_author = request.form['author']
+        new_post = BlogPost(title=post_title, content = post_content, author = post_author)
         db.session.add(new_post)
         db.session.commit()
-        return redirect('posts')
+        return redirect('/posts')
     else:
         all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
-        return render_template('posts.html', posts = all_posts)
 
-@app.route('/home')
-@app.route('/home/<id>')
-def hello(id=None):
-    return "Hello, " + str(id)
-
-
-@app.route('/onlyget', methods = ['POST', 'GET'])
-def get_req():
-    return 'You can only get this webpage.'
-
-@app.route('/name/<username>')
-def get_username(username = ''):
-    return render_template('index.html', username = username)
+    return render_template('posts.html', posts = all_posts)
 
 @app.route('/posts/delete/<int:id>')
 def delete(id):
@@ -57,19 +75,33 @@ def delete(id):
     db.session.commit()
     return redirect('/posts')
 
-@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+@app.route('/posts/edit/<int:id>', methods = ['GET', 'POST'])
 def edit(id):
+    
     post = BlogPost.query.get_or_404(id)
     if request.method == 'POST':
         post.title = request.form['title']
-        post.author = request.form['author']
         post.content = request.form['content']
+        post.author = request.form['author']
         db.session.commit()
         return redirect('/posts')
     else:
-        return render_template('edit.html', post=post)
+        return render_template('edit.html', post = post)
+
+#Login
+@app.route('/login', methods = ['POST'])
+def login():
+    #Info from form
+    username = request.form['username']
+    password = request.form['password']
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        session['username'] = username
+        return redirect(url_for('dashboard'))
+    else:
+        return render_template('index.html')
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
-
-#this is skibidi
