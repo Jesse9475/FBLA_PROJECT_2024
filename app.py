c18b01701f700ebc350051464cfe95e8ce62a7bc
SoleDuck <wis2017wa@gmail.com>
2025-01-14 18:58:52 -0800

    Added an authentication system, added route dashboard, login, and register. Removed login.html file because it is not needed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -6,8 +6,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 #Authenticator
 app = Flask(__name__)
 app.secret_key = "RNX73oMEeL5ShKoaHiiZkw"
-
-
 
 #Post database model
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
@@ -25,10 +23,10 @@ class User(db.Model):
     __bind_key__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(25), unique = True, nullable = False)
-    password = db.Column(db.String(150), nullable = False)
+    password_hash = db.Column(db.String(150), nullable = False)
 
     def set_password(self, password):
-        self.password_hash = generate_passowrd_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
@@ -75,7 +73,7 @@ def delete(id):
     db.session.commit()
     return redirect('/posts')
 
-@app.route('/posts/edit/<int:id>', methods = ['GET', 'POST'])
+@app.route('/posts/edit/<int:id>', methods = ['POST'])
 def edit(id):
     
     post = BlogPost.query.get_or_404(id)
@@ -89,17 +87,43 @@ def edit(id):
         return render_template('edit.html', post = post)
 
 #Login
-@app.route('/login', methods = ['POST'])
+@app.route("/login", methods = ['POST'])
 def login():
     #Info from form
     username = request.form['username']
-    password = request.form['password']
+    password = request.form["password"]
     user = User.query.filter_by(username=username).first()
     if user and user.check_password(password):
         session['username'] = username
         return redirect(url_for('dashboard'))
     else:
-        return render_template('index.html')
+        return render_template('index.html', error = 'Invalid username or password.')
+
+@app.route("/register", methods = ['POST'])
+def register():
+    username = request.form['username']
+    password = request.form["password"]
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return render_template("index.html", error = "User already here!")
+    else:
+        new_user = User(username = username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        session['username'] = username
+        return redirect(url_for('dashboard'))
+
+@app.route("/dashboard")
+def dashboard():
+    if "username" in session:
+        return render_template("dashboard.html", username = session['username'])
+    return redirect(url_for('index'))
+
+@app.route("/logout")
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     with app.app_context():
