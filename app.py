89a1ae5aa6cb19a7e7fafd4b0c34726e4b70b09b
SoleDuck <wis2017wa@gmail.com>
2025-01-14 19:54:42 -0800

    .

---
+++ app.py
@@ -0,0 +1,131 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+#Authenticator
+app = Flask(__name__)
+app.secret_key = "RNX73oMEeL5ShKoaHiiZkw"
+
+#Post database model
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+
+app.config['SQLALCHEMY_BINDS'] = {
+
+    'users': 'sqlite:///users.db'
+
+}
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+#Database model
+class User(db.Model):
+    __bind_key__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(25), unique = True, nullable = False)
+    password_hash = db.Column(db.String(150), nullable = False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+#Blog model
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    content = db.Column(db.Text, nullable = False)
+    author = db.Column(db.String(20), nullable = False, default = 'N/A')
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog post ' + str(self.id) 
+
+all_posts = []
+
+@app.route('/')
+def index():
+    if "username" in session:
+        return redirect(url_for('dashboard'))
+    return render_template('index.html')
+
+@app.route('/posts', methods=['GET', 'POST'])
+def posts():
+
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author = request.form['author']
+        new_post = BlogPost(title=post_title, content = post_content, author = post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+
+    return render_template('posts.html', posts = all_posts)
+
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
+
+@app.route('/posts/edit/<int:id>', methods = ['POST'])
+def edit(id):
+    
+    post = BlogPost.query.get_or_404(id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+        post.author = request.form['author']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post = post)
+
+#Login
+@app.route("/login", methods = ['POST'])
+def login():
+    #Info from form
+    username = request.form['username']
+    password = request.form["password"]
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        session['username'] = username
+        return redirect(url_for('dashboard'))
+    else:
+        return render_template('index.html', error = 'Invalid username or password.')
+
+@app.route("/register", methods = ['POST'])
+def register():
+    username = request.form['username']
+    password = request.form["password"]
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return render_template("index.html", error = "User already here!")
+    else:
+        new_user = User(username = username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        session['username'] = username
+        return redirect(url_for('dashboard'))
+
+@app.route("/dashboard")
+def dashboard():
+    if "username" in session:
+        return render_template("dashboard.html", username = session['username'])
+    return redirect(url_for('index'))
+
+@app.route("/logout")
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
