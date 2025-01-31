76dbf6925fa836775170301506f5a856d60d0db6
Surya-Dusi <shravantsurya@icloud.com>
2025-01-30 19:18:29 -0800

    frontend

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ class User(db.Model):
     password_hash = db.Column(db.String(150), nullable = False)
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password, method='pbkdf2')
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
